03dc838108725390ae42715e36522787282a3828
nmema <nicolas.a.mema@gmail.com>
2023-02-05 13:53:13 -0300

    #5 add inflation feature

--- src/ecs/argentina-bot/app.py
+++ src/ecs/argentina-bot/app.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from utils.get_token import BOT_TOKEN
+from utils.get_inflation import handler
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -9,10 +10,30 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text='Bienvenido a Argentina con Datos!'
     )
 
+async def inflation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        requested_period = context.args[0]
+        inflation_rate = handler(requested_period)
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=inflation_rate
+        )
+
+    except:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Algo salió mal \U0001F61E por favor intente de nuevo.'
+        )
+
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    
+    inflation_handler = CommandHandler('inflacion', inflation)
+    application.add_handler(inflation_handler)
     
     application.run_polling()
