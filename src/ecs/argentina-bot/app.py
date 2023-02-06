4ead95cc2c3c24cfc77d16e7a7a6dab7962009d1
nmema <nicolas.a.mema@gmail.com>
2023-02-06 13:17:37 -0300

    #9 add feedback command

--- src/ecs/argentina-bot/app.py
+++ src/ecs/argentina-bot/app.py
@@ -1,12 +1,22 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes, 
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters
+)
 
 import boto3
+import datetime
 import json
 import os
 
 from utils.get_token import BOT_TOKEN
 
+
+FEEDBACK = 0
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -39,6 +49,50 @@ async def inflation(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text='Algo salió mal \U0001F61E por favor intente de nuevo.'
         )
 
+async def feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text(
+        "Este Bot se encuentra en desarrollo!\n\n"
+        "Dejame tu comentario de que te pareció la experiencia, "
+        "y de nuevas funcionalidades que te gustaria ver.\n"
+        "A mí gustaria tener nuevos comandos \U0001F60E"
+    )
+    return FEEDBACK
+
+async def save_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    user_id = user['id']
+    chat_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    message = update.message.text
+    
+    try:
+        client = boto3.client('dynamodb', region_name='us-west-2')
+        client.put_item(
+            TableName='argcd-feedback-test',  # TODO: environment variable
+            Item={
+                'user_id': {'N': str(user_id)},
+                'chat_date': {'S': chat_date},
+                'message': {'S': message}
+            }
+        )
+        
+        await update.message.reply_text(
+            'Listo! Muchas gracias por tu comentario \U0001F929'
+        )
+
+    except:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Algo salió mal \U0001F61E por favor intente de nuevo.'
+        )
+        
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text(
+        "Comentario cancelado."
+    )
+
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
@@ -49,5 +103,14 @@ if __name__ == '__main__':
     
     inflation_handler = CommandHandler('inflacion', inflation)
     application.add_handler(inflation_handler)
+    
+    feedback_handler = ConversationHandler(
+        entry_points=[CommandHandler('comentario', feedback)],
+        states={
+            FEEDBACK: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_feedback)]
+        },
+        fallbacks=[CommandHandler("cancel", cancel)]
+    )
+    application.add_handler(feedback_handler)
     
     application.run_polling()
